0dec0d172ab8e7e5703dc541b627c87af8363c37
chris.miranda <christophermiranda@pursuit.org>
2023-12-13 17:12:55 -0500

    organized everything

--- src/Components/Attendees.jsx
+++ src/Components/Attendees.jsx
@@ -1,3 +1,16 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee"
+export default function Attendees({toggleEventAttendees, showAttendees, attendees}) {
+  return (<>
+    <button onClick={toggleEventAttendees}>
+      {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+    </button>
+
+    {showAttendees ? (
+      <div className="attendees">
+        {attendees.map((attendee, index) => (
+          <Attendee key={attendee.id} attendee={attendee} />
+        ))}
+      </div>
+    ) : null}
+  </>)
 }
