34b7bc611c9cf87390df0fa150228dc569edab49
Jose Barrios <josebarrios@pursuit.org>
2023-12-13 18:16:00 -0500

    Finished lab

--- src/Components/Attendees.jsx
+++ src/Components/Attendees.jsx
@@ -1,3 +1,31 @@
-export default function Attendees() {
-  return;
+import React, { useState } from "react";
+import Attendee from "./Attendee";
+
+export default function Attendees({ attendees, updateEventAttendance, event }) {
+  const [showAttendees, setShowAttendees] = useState(false);
+
+  const toggleAttendees = () => {
+    setShowAttendees(!showAttendees)
+  }
+
+  return (
+    <>
+      <button onClick={toggleAttendees}>
+        {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+      </button>
+
+      {showAttendees && (
+        <div className="attendees">
+          {attendees.map((attendee) => (
+            <Attendee
+              key={attendee.id}
+              attendee={attendee}
+              updateEventAttendance={updateEventAttendance}
+              event={event}
+            />
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
