2ab3ef849d64cfb1fc40150ab2eaeccac1c9d092
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2023-12-13 17:35:00 -0500

    Updated and moved most i could without breaking it

--- src/Components/Attendees.jsx
+++ src/Components/Attendees.jsx
@@ -1,3 +1,20 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee";
+export default function Attendees({showAttendees,attendees,updateEventAttendance,event}) {
+  
+  return(
+    <>
+    {showAttendees ? (
+      <div className="attendees">
+        {attendees.map((attendee, index) => (
+          <Attendee
+            key={attendee.id}
+            attendee={attendee}
+            updateEventAttendance={updateEventAttendance}
+            event={event}
+          />
+        ))}
+      </div>
+    ) : null}
+  </>
+);
 }
