91f8eecb57eb3003de9bc740947e7b1739415928
Isiah Arrufat <isiaharrufat@pursuit.org>
2023-12-14 05:00:13 -0500

    lab partically completed

--- src/Components/Attendees.jsx
+++ src/Components/Attendees.jsx
@@ -1,3 +1,27 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee";
+import { useState } from "react";
+
+export default function Attendees({attendees, event, updateEventAttendance}) {
+  const [showAttendees, setShowAttendees] = useState(false);
+
+  function toggleEventAttendees() {
+    setShowAttendees(!showAttendees);
+  }
+
+  return (
+    <>
+    <button onClick={toggleEventAttendees}>
+      {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+    </button>
+
+    {showAttendees ? (
+      <div className="attendees">
+        {attendees.map((attendee, index) => {
+          return <Attendee event= {event} index= {index} attendee= {attendee} key= {attendee.id} updateEventAttendance= {updateEventAttendance}/>
+        }
+        )}
+      </div>
+    ) : null}
+    </>               
+  )
 }
