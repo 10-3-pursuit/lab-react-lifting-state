a2da9699074db8db8158f17d38ded0883cba8593
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2023-12-30 16:05:01 -0500

    completed lab

--- src/Components/Attendees.jsx
+++ src/Components/Attendees.jsx
@@ -1,3 +1,24 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee"
+
+export default function Attendees({ event, attendees, toggleEventAttendees, updateEventAttendance, showAttendees}) {
+  return(
+    <>
+    <button onClick={toggleEventAttendees}>
+      {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+    </button>
+
+    {showAttendees ? (
+      <div className="attendees">
+        {attendees.map((attendee, index) => (
+          <Attendee 
+            key={index}
+            attendee={attendee}
+            event={event}
+            updateEventAttendance={updateEventAttendance}
+          />
+        ))}
+      </div>
+    ) : null}
+  </>
+  )
 }
